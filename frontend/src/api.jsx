59aea182809534b42ac6b0c1d995894a1936a1cd
Sarthak Devlal <sarthakdevlal22@gmail.com>
2025-09-28 19:03:46 +0530

    Update api file

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: "http://localhost:8000"
-  baseURL: process.env.REACT_APP_API_URL
+  // baseURL: process.env.REACT_APP_API_URL
+  baseURL: "https://grocerynetwithchatbot.onrender.com"
 });
 
 export default api;
