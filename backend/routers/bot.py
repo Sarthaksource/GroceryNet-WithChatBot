bf41637cfdc56faefbdb96d56ac823ddc041379b
Sarthak Devlal <sarthakdevlal22@gmail.com>
2025-09-28 21:30:01 +0530

    Update bot file

--- backend/routers/bot.py
+++ backend/routers/bot.py
@@ -10,10 +10,6 @@ import redis
 import os
 import json
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
-
 r = redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
 
 SESSION_EXPIRE_SECONDS = 3600  # 1 hour expiration for sessions
